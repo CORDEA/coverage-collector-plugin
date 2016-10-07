f8c831e5ee15b74c33786124d4676a65269787e4
Yoshihiro Tanaka <contact@cordea.jp>
2016-10-07 09:05:02 +0900

    receive coverage report

--- src/main/java/jp/cordea/coveragecollector/TemplateLoader.java
+++ src/main/java/jp/cordea/coveragecollector/TemplateLoader.java
@@ -4,8 +4,9 @@ import com.mitchellbosecke.pebble.PebbleEngine;
 import com.mitchellbosecke.pebble.error.PebbleException;
 import com.mitchellbosecke.pebble.loader.StringLoader;
 import com.mitchellbosecke.pebble.template.PebbleTemplate;
-import jp.cordea.coveragecollector.model.TestSuite;
-import jp.cordea.coveragecollector.model.TestTotal;
+import jp.cordea.coveragecollector.model.coverage.Report;
+import jp.cordea.coveragecollector.model.coverage.Summary;
+import jp.cordea.coveragecollector.model.test.TestCase;
 
 import java.io.IOException;
 import java.io.StringWriter;
@@ -18,16 +19,22 @@ import java.util.Map;
  */
 public class TemplateLoader {
 
-    private List<TestSuite> testSuites;
-    private List<TestSuite> masterTestSuites;
-    private TestTotal testTotal;
-    private TestTotal masterTestTotal;
+    private Report report;
 
-    public TemplateLoader(List<TestSuite> testSuites, List<TestSuite> masterTestSuites, TestTotal testTotal, TestTotal masterTestTotal) {
-        this.testSuites = testSuites;
-        this.masterTestSuites = masterTestSuites;
-        this.testTotal = testTotal;
-        this.masterTestTotal = masterTestTotal;
+    private Summary summary;
+
+    private Report masterReport;
+
+    private Summary masterSummary;
+
+    private List<TestCase> failureTestCases;
+
+    public TemplateLoader(Report report, Summary summary, Report masterReport, Summary masterSummary, List<TestCase> failureTestCases) {
+        this.report = report;
+        this.summary = summary;
+        this.masterReport = masterReport;
+        this.masterSummary = masterSummary;
+        this.failureTestCases = failureTestCases;
     }
 
     public String load(String templateString) {
@@ -41,12 +48,22 @@ public class TemplateLoader {
         StringWriter writer = new StringWriter();
 
         Map<String, Object> context = new HashMap<>();
-        context.put("testSuites", testSuites);
-        context.put("testTotal", testTotal);
-        if (masterTestSuites != null) {
-            context.put("masterTestSuites", masterTestSuites);
-            context.put("masterTestTotal", masterTestTotal);
+        if (report != null) {
+            context.put("report", report);
+            context.put("summary", summary);
         }
+        if (masterReport != null) {
+            context.put("masterReport", masterReport);
+            context.put("masterSummary", masterSummary);
+        }
+        if (failureTestCases != null) {
+            context.put("failureTestCases", failureTestCases);
+        }
+        boolean failure = false;
+        if (report == null && masterReport == null) {
+            failure = true;
+        }
+        context.put("failure", failure);
 
         try {
             template.evaluate(writer, context);
